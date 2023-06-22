ac7e82e16b8c01fdd699c08f4f7d8dd9759b1c29
ZeroCool989 <>
2023-06-22 15:55:17 +0000

    Add products to product category

--- assets/js/script copy.js
+++ assets/js/script copy.js
@@ -1,54 +1,38 @@
 // Filter Selection
-// First, select all the buttons
-const buttons = document.querySelectorAll("#filterButtons .btn");
+filterSelection('all');
 
-// Function to remove the 'active' class from all buttons
-function removeActiveClass() {
-    buttons.forEach((btn) => {
-        btn.classList.remove('active');
-    });
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("product");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
 }
 
-// Function to handle the click event
-function handleButtonClick(event) {
-    // Remove 'active' class from all buttons
-    removeActiveClass();
-
-    // Add 'active' class to the clicked button
-    event.target.classList.add('active');
-
-    // Get the filter from the button's onclick attribute
-    let filter = event.target.getAttribute('onclick');
-    filter = filter.replace("filterSelection('", "").replace("')", "");
-
-    // Select all products
-    let products = document.querySelectorAll(".product");
-
-    // Hide all products
-    products.forEach((product) => {
-        product.style.display = 'none';
-    });
-
-    // If filter is 'all', show all products
-    if (filter === 'all') {
-        products.forEach((product) => {
-            product.style.display = 'block';
-        });
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
-    // Otherwise, show only the products that have the class corresponding to the filter
-    else {
-        let filteredProducts = document.querySelectorAll(`.product.${filter}`);
-        filteredProducts.forEach((product) => {
-            product.style.display = 'block';
-        });
-    }
+  }
 }
 
-// Assign the click event to each button
-buttons.forEach((btn) => {
-    btn.addEventListener('click', handleButtonClick);
-});
-
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("filterButtons");
